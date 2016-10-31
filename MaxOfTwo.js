88f52336117f2b1345fdd44c9af89acd771bfcfb
Alice Makaimura <arumumu@live.jp>
2016-10-31 10:07:38 +0900

    Create MaxOfTwo.js

---
+++ MaxOfTwo.js
@@ -0,0 +1,17 @@
+"use strict";
+
+function myMax(a, b) {
+    return Math.max(a, b);
+}
+
+/*
+var assert = require('assert');
+
+if (!global.is_checking) {
+    // These "asserts" using only for self-checking and not necessary for auto-testing
+    assert(myMax(3, 3) == 3, "First");
+    assert(myMax(0, 1) == 1, "Second");
+    assert(myMax(3, 2) == 3, "Third");
+    console.log("All set? Click \"Check\" to review your code and earn rewards!");
+}
+*/
